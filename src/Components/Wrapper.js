77375d6b923f58f04e621e7949fef02b286064cc
Samsegun <oyebadesamsegun@gmail.com>
2022-04-20 11:47:54 +0100

    responsive layout

--- src/Components/Wrapper.js
+++ src/Components/Wrapper.js
@@ -1,11 +1,7 @@
 const Wrapper = props => {
   const styles = props.styles ? props.styles : "";
 
-  return (
-    <div className={"mt-12 flex-wrap xs:gap-24 " + styles}>
-      {props.children}
-    </div>
-  );
+  return <div className={"mt-12 flex-wrap " + styles}>{props.children}</div>;
 };
 
 export default Wrapper;
